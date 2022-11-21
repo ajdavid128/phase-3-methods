886d0a3e467a468c099a64fa1994d000871996fc
Aaron David <ajdavid128@gmail.com>
2022-11-20 18:21:00 -0600

    commit tree 3beaeb9ed264382ba5b26ebe2abe93cb717f7cb6

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet "Bob"
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default "Dude"
+
+def add num1, num2 
+    num1 + num2
+end
+
+add 1, 2
+
+def halve number
+    if number.class != Integer
+        return nil
+    end
+
+    number/2
+end
+
+halve 6
+# halve "six"
